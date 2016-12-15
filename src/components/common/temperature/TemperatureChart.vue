5a7adb06510a3fdcfc14b4e73725996af38ff803
Jack Yang <ygjack414@hotmail.com>
2016-12-15 09:57:01 +0800

    complete temp chart

--- src/components/common/temperature/TemperatureChart.vue
+++ src/components/common/temperature/TemperatureChart.vue
@@ -7,47 +7,32 @@
   import Highcharts from 'highcharts'
   import moment from 'moment'
 
-  const testData = [{
-    temperature: 36.3, period: false, date: new Date(2016, 11, 4)
-  }, {
-    temperature: 36.3, period: false, date: new Date(2016, 11, 6)
-  }, {
-    temperature: 36.5, period: false, date: new Date(2016, 11, 7)
-  }, {
-    temperature: 37.1, period: true, date: new Date(2016, 11, 8)
-  }, {
-    temperature: 37.0, period: true, date: new Date(2016, 11, 9)
-  }, {
-    temperature: 36.4, period: false, date: new Date(2016, 11, 10)
-  }]
-
   /**
    * fill the missing data with null temperature
    */
   function enrichTemperatureRecords (temperatureRecords, startDate, totalDays) {
     var records = []
     for (let i = 0; i < totalDays; i++) {
-      let record = temperatureRecords[i]
       let date = moment(startDate).add(i, 'days').toDate()
+      let record = temperatureRecords.filter(r => {
+        return moment(r.date).isSame(date)
+      })[0]
 
-      if (!record || !moment(record.date).isSame(date)) {
-        /*
-        temperatureRecords.splice(i, 0, {
-          temperature: null,
-          period: false,
-          date: date
+      if (record) {
+        records.push({
+          temperature: record.temperature,
+          period: record.period,
+          date: record.date
         })
-        */
+      } else {
         records.push({
           temperature: null,
           period: false,
           date: date
         })
-      } else {
-        records.push(temperatureRecords[i])
       }
     }
-    console.log(records)
+
     return records
   }
 
@@ -124,6 +109,7 @@
         enabled: false
       },
       tooltip: {
+        enabled: false,
         crosshairs: true,
         shared: true
       },
@@ -149,8 +135,32 @@
   }
 
   export default {
+    props: {
+      temperatureRecords: {
+        type: Array,
+        default () {
+          return [{
+            temperature: 36.3, period: false, date: new Date(2016, 11, 4)
+          }, {
+            temperature: 36.3, period: false, date: new Date(2016, 11, 6)
+          }, {
+            temperature: 36.5, period: false, date: new Date(2016, 11, 7)
+          }, {
+            temperature: 37.1, period: true, date: new Date(2016, 11, 8)
+          }, {
+            temperature: 37.0, period: true, date: new Date(2016, 11, 9)
+          }, {
+            temperature: 36.4, period: false, date: new Date(2016, 11, 10)
+          }]
+        }
+      },
+      mode: {
+        type: String,
+        default: 'week'
+      }
+    },
     mounted () {
-      Highcharts.chart(this.$refs.chartMountNode, createChartOptions(testData, 'week'))
+      Highcharts.chart(this.$refs.chartMountNode, createChartOptions(this.temperatureRecords, this.mode))
     }
   }
 </script>
